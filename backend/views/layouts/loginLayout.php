f1eaf968b8441d00767538f050e3d4fd3f4899c3
oneword <kaifeng223@sina.com>
2016-01-25 14:00:58 +0800

    第38节视频，简单修饰（实际上是删）自定义layout的页面代码

--- backend/views/layouts/loginLayout.php
+++ backend/views/layouts/loginLayout.php
@@ -3,14 +3,14 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use backend\assets\AppAsset;
+use backend\assets\LoginAsset;
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use common\widgets\Alert;
 
-AppAsset::register($this);
+LoginAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
